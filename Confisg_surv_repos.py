e82148f7f6848dd10c373f8beb809929f92b5aa5
dima.borovsky <dimab1143@gmail,com>
2024-09-29 12:21:14 +0300

    "updated the HTML elements"

--- Confisg_surv_repos.py
+++ Confisg_surv_repos.py
@@ -1,17 +1,27 @@
 from selenium import webdriver
 import time
-server_name = #server names will appear here in a large string unless any better way to do it
+
+pop = ['''AKL,AMS,ATL,BCN,BKK,BOG,BOM,CDG,CGK,CHI,CPH,CPT,DEN,DET,DFW,DUB,DUS,DXB,EWR,EZE,FRA,GRU,HAN,HKG,HOU,IAD,IST,JKT,JNB,KOR,KUL,LAX,LHR,LIM,LON,MAD,MED,MEL,MEX,MIA,MNL,MSK,MXP,NDL,NYC,ORD,OSK,QRO,RIO,SAO,SCL,SEA,SIN,SJC,STC,STO,SYD,TKO,TOR,TPE,VAN,VIE,WAR,YUL,ZRH
+''']
+
+
+server_to_zeroise = input(f"which server needs to be zeroised.{pop}")
+#server names will appear here in a large string unless any better way to do it for ogw,s
 
 My_driver = webdriver.Chrome()
-My_driver.get(f"https://zamsh-incap.{server_name}.infra-net.mobi")
+My_driver.get(f"https://{server_to_zeroise}.{pop}.zamsh.incapsula.mobi/config_survivability_change_revlite_repository?global=0&sites_and_accounts=0&confirm=yes")
 
-#find the HTML value of the repos that needs to be zeroised
-My_driver.find_element(by=,value=).send_keys("0")
+My_driver.get(server_to_zeroise)
 
-#finds the HTML value of the repos that needs to be zeroised
-My_driver.find_element(by=,value=).send_keys("0")
+# finds the survivabilty tab and cliks the dropdown
+My_driver.find_element(by="id",value="headingThree").click()
 
-My_driver.find_element(by=, value=).click()
+#find the HTML value of the global repos to zeroise
+My_driver.find_element(by="type",value="global").send_keys("0")
+
+#finds the HTML value of the S&A repos to zeroise
+My_driver.find_element(by="type",value="sites_and_accounts").send_keys("0")
+#keeps the browswer up for 5 minutes in order to verify keys are being sent
 time.sleep(5)
 
-actual= My_driver.find_element(by=,value).text
+
